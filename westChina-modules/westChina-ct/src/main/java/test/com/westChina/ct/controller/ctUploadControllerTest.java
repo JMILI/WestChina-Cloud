dffd9e1e6238c553bf916bd207ddea9c48ba616f
JMILI <923749851@qq.com>
2023-02-22 16:14:51 +0800

    修改数据库sql信息

---
+++ westChina-modules/westChina-ct/src/main/java/test/com/westChina/ct/controller/ctUploadControllerTest.java
@@ -0,0 +1,64 @@
+package test.com.westChina.ct.controller;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * ctUploadController Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>2�� 14, 2022</pre>
+ */
+public class ctUploadControllerTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: ctFile(MultipartFile file)
+     */
+    @Test
+    public void testCtFile() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: readDicomInfo(MultipartFile file)
+     */
+    @Test
+    public void testReadDicomInfo() throws Exception {
+//        DicomInputStream dis = new DicomInputStream(new File("D:/000-inbox/Desktop/3_2.dcm"));
+////        Attributes d = dis.readDataset(-1, -1);
+////        Attributes dd=dis.getFileMetaInformation();
+////        Attributes ddd=dis.readDatasetUntilPixelData();
+////        Attributes dddd=dis.readItem();
+////        String pn = tranByte2String(d.getBytes(Tag.PatientName));
+////        System.out.println(pn);
+//
+////        FileInputStream fis = new FileInputStream("sd");
+//        Attributes a=dis.readDataset();
+//        System.out.println(a.getString(Tag.PatientName));
+
+    }
+
+
+    /**
+     * Method: transferToFile(MultipartFile multipartFile)
+     */
+    @Test
+    public void testTransferToFile() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 
